1c01025c42b4bb7cc25a4607ae1997d92204f1b3
Master Mind <48193931+iboss21@users.noreply.github.com>
2025-01-19 21:33:45 -0600

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// src/App.tsx
+import { ConverterSteps } from './components/cv-converter/converter-steps';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-600 to-blue-300">
+      <ConverterSteps />
+    </div>
+  );
+}
